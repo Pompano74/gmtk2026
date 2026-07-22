001c3e0ee1673f5138eb8e913b00222f035d8ceb
Antoine Janelle <2325061@bdeb.qc.ca>
2026-07-22 19:53:43 -0400

    tempo_global finish v1

--- script/global/tempo_global.gd
+++ script/global/tempo_global.gd
@@ -1,1 +1,17 @@
 extends Node
+
+@export var bpm: float = 120
+var beat_timer: float
+@onready var sound: FmodEventEmitter2D = $FmodEventEmitter2D
+
+
+func _ready() -> void:
+	beat_timer = 1 / (bpm / 60)
+
+func _process(delta: float) -> void:
+	
+	if beat_timer <= 0.0:
+		sound.play()
+		beat_timer = 1 / (bpm / 60)
+	else:
+		beat_timer = beat_timer - delta
