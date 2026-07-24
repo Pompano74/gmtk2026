7a8d5fe03200b70689285edbbad6f7cedc2a14c9
Antoine Janelle <2325061@bdeb.qc.ca>
2026-07-24 12:21:39 -0400

    count_plus logic finish for player / need logic for enemy / need visual feedback

--- script/level_tiles/count_plus.gd
+++ script/level_tiles/count_plus.gd
@@ -6,14 +6,17 @@ extends InteractionTile
 func _ready() -> void:
 	plus_value += 1
 	destroy_on_interaction = true
+	can_enemy_interact_by_landing_on_tile = true
+	can_player_interact_by_landing_on_tile = true
+	can_player_interact_by_shooting_tile = true
 	
 func on_player_enters_tile(player: PlayerCharacter) -> void:
 	TempoGlobal.coutdown_value += plus_value
-	print("p_cooldown +")
+	super(player)
 
 func on_player_bullet_enters_tile(bullet: Node2D) -> void:
 	TempoGlobal.coutdown_value += plus_value
-	print("b_cooldown +")
+	super(bullet)
 
 func on_enemy_enters_tile(enemy: BaseEnemy) -> void:
-	print("enemy_destroyed")
+	super(enemy)
