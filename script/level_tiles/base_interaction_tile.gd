3de149352c23fd7558b83dece8a3a7b2e653b89f
Éloi <eloijutras@gmail.com>
2026-07-24 11:19:33 -0400

    Set up interaction tiles

---
+++ script/level_tiles/base_interaction_tile.gd
@@ -0,0 +1,33 @@
+extends Node2D
+class_name InteractionTile
+
+@onready var collision := $CollisionShape2D
+@export var can_player_interact_by_landing_on_tile := false
+@export var can_player_interact_by_shooting_tile := false
+@export var can_enemy_interact_by_landing_on_tile := false
+@export var destroy_on_interaction := false
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	var player = body as PlayerCharacter
+	if is_instance_valid(player):
+		if can_player_interact_by_landing_on_tile:
+			on_player_enters_tile(player)
+			return
+	if body.is_in_group("PlayerBullet"):
+		if can_player_interact_by_shooting_tile:
+			on_player_bullet_enters_tile(body)
+			return
+	var enemy = body as BaseEnemy
+	if is_instance_valid(enemy):
+		if can_enemy_interact_by_landing_on_tile:
+			on_enemy_enters_tile(enemy)
+			return
+
+func on_player_enters_tile(player: PlayerCharacter) -> void:
+	pass
+
+func on_player_bullet_enters_tile(bullet: Node2D) -> void:
+	pass
+
+func on_enemy_enters_tile(enemy: BaseEnemy) -> void:
+	pass
