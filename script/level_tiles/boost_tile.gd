2850add029d1cd7cbf2ef63a1a13155f30107b39
Antoine Janelle <2325061@bdeb.qc.ca>
2026-07-26 09:43:46 -0400

    ALL LEVEL ARE DONE!

--- script/level_tiles/boost_tile.gd
+++ script/level_tiles/boost_tile.gd
@@ -69,16 +69,20 @@ func on_player_bullet_enters_tile(bullet: Node2D) -> void:
 	print("tile_rotation:",round(global_rotation_degrees))
 	if global_rotation_degrees == 0: #up
 		bullet.get_parent().dir = Vector2(0 , -1)
+		bullet.get_parent().animated_sprite_2d.play("Up")
 		print("UP")
 	
 	elif global_rotation_degrees == -90: #left
 		bullet.get_parent().dir = Vector2(-1 , 0)
+		bullet.get_parent().animated_sprite_2d.play("Left")
 		print("LEFT")
 	elif global_rotation_degrees == 90: #right
 		bullet.get_parent().dir = Vector2(1 , 0)
+		bullet.get_parent().animated_sprite_2d.play("Right")
 		print("RIGHT")
 	else: #down
 		bullet.get_parent().dir = Vector2(0 , 1)
+		bullet.get_parent().animated_sprite_2d.play("Down")
 		print("DOWN")
 	print("new_dir:",bullet.get_parent().dir)
 	if is_wall:
